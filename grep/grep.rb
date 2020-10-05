2fc10a4f1798cec552f11e0b878a5da53a3ff5d2
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-05 17:12:18 +0300

    feat: killed all friday and half monday on grep task

---
+++ grep/grep.rb
@@ -0,0 +1,13 @@
+require_relative ('flags.rb')
+
+class Grep
+    def self.grep(pattern, flags, files)
+        Flags.new.which_flag(files, flags, pattern)
+    end
+end
+
+files = ["text.txt", "text1.txt"]
+flags = []
+pattern = "hello"
+
+Grep.grep(pattern, flags, files)
