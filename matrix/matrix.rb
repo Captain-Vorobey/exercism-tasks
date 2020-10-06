3cd2e70199d6287207b97976c6aa12c255b36010
Captain-Vorobey <43318298+Captain-Vorobey@users.noreply.github.com>
2020-10-06 15:57:33 +0300

    Update matrix/matrix.rb
    Co-authored-by: dzmitry aleinik <42341664+dzmitry-aleinik-itechart@users.noreply.github.com>

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -10,7 +10,7 @@ class Matrix
     attr_reader :rows, :columns
   
     def initialize(matrix)
-      @rows = matrix.split("\n").map{|row| row.split(' ').map(&:to_i)}
+      @rows = matrix.split("\n").map{|row| row.split.map(&:to_i)}
       @columns = rows.transpose
     end
   end
