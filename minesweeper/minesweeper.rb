593a80b69f0b5550be24c6c87802d179ae8c6479
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-08 18:05:15 +0300

    Added rubocop for style guide

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -1,39 +1,34 @@
-=begin
-Write your code for the 'Minesweeper' exercise in this file. Make the tests in
-`minesweeper_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/minesweeper` directory.
-=end
+# Write your code for the 'Minesweeper' exercise in this file. Make the tests in
+# `minesweeper_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/minesweeper` directory.
 
 class Board
-    def self.transform(input)
-        @t = input.map { |el| el.split('') }
-        
-        @t.each_with_index do |r, r_in| 
-            r.each_with_index do |c, c_in| 
-                if mine?(c) 
-                    insert_one(r_in, c_in)
-                end    
-            end
-        end
-        @t.map { |el| el.join('') }
-    end
+  def self.transform(input)
+    @t = input.map { |el| el.split('') }
 
-    def self.mine?(c)
-        c.include?('*')
+    @t.each_with_index do |r, r_in|
+      r.each_with_index do |c, c_in|
+        insert_one(r_in, c_in) if mine?(c)
+      end
     end
+    @t.map { |el| el.join('') }
+  end
 
-    def self.digit?(ch)
-        ch.match(/[1-8 ]/) ? "#{ch.to_i + 1}" : ch
-    end
+  def self.mine?(c)
+    c.include?('*')
+  end
 
-    def self.insert_one(r, c)
-        (r-1..r+1).each { |i|
-            (c-1..c+1).each { |j|
-                @t[i][j] = digit?(@t[i][j])
-            }
-        }
+  def self.digit?(ch)
+    ch.match(/[1-8 ]/) ? (ch.to_i + 1).to_s : ch
+  end
+
+  def self.insert_one(r, c)
+    (r - 1..r + 1).each do |i|
+      (c - 1..c + 1).each do |j|
+        @t[i][j] = digit?(@t[i][j])
+      end
     end
+  end
 end
-
